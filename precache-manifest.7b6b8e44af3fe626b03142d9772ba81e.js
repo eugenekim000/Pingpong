268961a94f781c8ee26afa002a497cb2924e3f0a
eugenekim000 <eugenekim000@gmail.com>
2020-06-24 09:58:06 -0700

    Updates

---
+++ precache-manifest.7b6b8e44af3fe626b03142d9772ba81e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8329cf255b52fd2e9bb2086a19c6315",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c5d85a6ac81e2dac9b2",
+    "url": "/static/css/main.fcdd092d.chunk.css"
+  },
+  {
+    "revision": "d966c629722f00ffa083",
+    "url": "/static/js/2.94a790f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94a790f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c5d85a6ac81e2dac9b2",
+    "url": "/static/js/main.76868c01.chunk.js"
+  },
+  {
+    "revision": "99ae3d96635ed41f24bc",
+    "url": "/static/js/runtime-main.d3be5fa9.js"
+  }
+]);
