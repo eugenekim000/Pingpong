57d6131d73e8cbd927a32134dafb018155cc17d6
eugenekim000 <eugenekim000@gmail.com>
2020-06-23 17:24:33 -0700

    test ball

--- src/Components/MainGame.js
+++ src/Components/MainGame.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { timeout, generateRobotReturnTime } from '../HelperFunctions';
 
 let timeouts = [];
@@ -8,6 +8,28 @@ export default function MainGame(props) {
 	const [score, setScore] = useState(0);
 	const [returned, setReturned] = useState(true);
 	const [playerSwing, setSwing] = useState(true);
+
+	const garden = useRef(null);
+	const ball = useRef(null);
+
+	var maxX = garden.clientWidth - ball.clientWidth;
+	var maxY = garden.clientHeight - ball.clientHeight;
+
+	window.addEventListener('deviceorientation', handleOrientation, true);
+	function handleOrientation(event) {
+		let x = event.beta;
+		let y = event.gamma;
+		if (x > 90) {
+			x = 90;
+		}
+		if (x < -90) {
+			x = -90;
+		}
+		x += 90;
+		y += 90;
+		ball.style.top = (maxY * y) / 180 - 10 + 'px';
+		ball.style.left = (maxX * x) / 180 - 10 + 'px';
+	}
 
 	const incomingAudio = new Audio('../public/ball-incoming.mp3');
 	const outgoingAudio = new Audio('../public/ball-outgoing.mp3');
@@ -55,6 +77,10 @@ export default function MainGame(props) {
 			{playerSwing && <button onClick={() => handleClick()}>Click me</button>}
 			{/* 			{<button onClick={() => handleAudio(outgoingAudio)}>test sound</button>}
 			 */}
+			<button onClick={(e) => console.log(handleOrientation())}>test</button>
+			<div ref={garden} class='garden'>
+				<div ref={ball} class='ball'></div>
+			</div>
 		</div>
 	);
 }
